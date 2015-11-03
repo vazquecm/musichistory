ef9140ac9c2c1ef101f30ec9f61184531154788c
Carmen <vazquecm@gmail.com>
2015-11-03 11:24:47 -0600

    initial

---
+++ javascripts/script.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+	var songListContainer = $("#song-list");
+
+	function dataConverter(rawJSONData) {
+		var songArray = rawJSONData.songs;
+
+		var songElementString = "";
+
+		for (var i = 0; i < songArray.length; i++) {
+			var currentSong = songArray[i];
+
+			songElementString = "<div>";
+			songElementString += "<h3>"+currentSong.title+"</h3>";
+			songElementString += "<p>"+currentSong.artist+"</p>";
+			songElementString += "<p>"+currentSong.album+"</p>";
+			songElementString += "<button class='delete'>Delete</button>";
+			songElementString += "</div>";
+
+			songListContainer.append(songElementString); 
+			console.log(currentSong);
+		};
+	}
+
+	$.ajax({
+		url: "data/songs.json"
+	}).done(dataConverter);
+
+	
+});
+
+// Delete button on "list music" song row  //
+
+	$(document).on('click', '.delete', function(event) {
+		event.target.remove("song-list");
+		console.log('click', event);
+});	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Button for "more-songs"  //
+
+// var moreSongs = "more-songs";
+
+// 	$(function(){
+//     	$("<button>Delete</button>").click(function(){
+//         var moreSongs= $("<input type='button' value='More Songs'>");
+//         console.log(moreSongs);
+//     });
+// });
+
+// 	$.ajax({
+//     url: "data/more-songs.json"
+//   }).done(dataConverter);
+
+// });
+
+
+// Adding music //
+
+// 	var addMusic = "Add Music";
+
+// 	$("#link-add").click(function(){
+// 	var addMusic = $(addMusic);
+// 	console.log(addMusic); 	    
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
